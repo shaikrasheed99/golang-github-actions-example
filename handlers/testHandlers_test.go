0cee76a4c7ff9a1220eef031ce5366530368fd74
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-10-03 22:34:28 +0530

    Add. Test case for second test api

--- handlers/testHandlers_test.go
+++ handlers/testHandlers_test.go
@@ -28,3 +28,21 @@ func TestFirstTestHandler(t *testing.T) {
 		assert.Equal(t, constants.FisrtTestAPIMessage, resMessage)
 	})
 }
+
+func TestSecondTestHandler(t *testing.T) {
+	t.Run("should be able return second test api with success message", func(t *testing.T) {
+		th := NewTestHandler()
+		router := gin.New()
+		router.GET(constants.SecondTestAPIPath, th.SecondTestHandler)
+
+		req, _ := http.NewRequest("GET", constants.SecondTestAPIPath, nil)
+		res := httptest.NewRecorder()
+		router.ServeHTTP(res, req)
+
+		var resMessage string
+		_ = json.Unmarshal(res.Body.Bytes(), &resMessage)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, constants.SecondTestAPIMessage, resMessage)
+	})
+}
